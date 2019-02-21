eef99054ff49655157a90435c9c365435a71c9a0
Dstar4 <danielstarling4@gmail.com>
2019-02-21 14:06:41 -0700

    searchbar css refactored into styled components

--- src/components/SearchBar/SearchBar.jsx
+++ src/components/SearchBar/SearchBar.jsx
@@ -1,14 +1,42 @@
 import React from "react";
-import "./SearchBar.css";
+// import "./SearchBar.css";
 import Iglogo from "../../assets/iglogo.png";
 import InstaCamera from "../../assets/instaCamera.svg";
+import styled from "styled-components";
+const Header = styled.div`
+  display: flex;
+  margin: 10px auto;
+  padding-bottom: 10px;
+  border-bottom: 1px solid darkgray;
+  justify-content: space-evenly;
+`;
+const Logos = styled.div`
+  display: flex;
+`;
+const IgCamera = styled.img`
+  height: 40px;
+  width: auto;
+  margin: 0 10px;
+`;
+const IgText = styled.img`
+  width: auto;
+  height: 40px;
+  padding: 0 10px;
+  border-left: 1px solid black;
+`;
+const Icons = styled.text`
+  display: flex;
+`;
+const Icon = styled.text`
+  margin: 0 10px;
+`;
 const SearchBar = props => {
   return (
-    <div className="search-bar-wrapper">
-      <div className="logos">
-        <img src={InstaCamera} alt="" className="ig-camera" />
-        <img src={Iglogo} alt="" className="ig-text" />
-      </div>
+    <Header>
+      <Logos>
+        <IgCamera src={InstaCamera} alt="" />
+        <IgText src={Iglogo} alt="" />
+      </Logos>
       <div className="search">
         <input
           type="text"
@@ -16,18 +44,18 @@ const SearchBar = props => {
           onKeyDown={props.searchPosts}
         />
       </div>
-      <div className="icons">
-        <div className="icon">
+      <Icons>
+        <Icon>
           <i className="far fa-compass" />
-        </div>
-        <div className="icon">
+        </Icon>
+        <Icon>
           <i className="far fa-heart" />
-        </div>
-        <div className="icon">
+        </Icon>
+        <Icon>
           <i className="far fa-user-circle" />
-        </div>
-      </div>
-    </div>
+        </Icon>
+      </Icons>
+    </Header>
   );
 };
 export default SearchBar;
